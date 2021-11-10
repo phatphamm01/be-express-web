bf79bed0d14c6f9a2fa7270a2ffb6d80413cf6d8
phatphamm01 <84543546+phatphamm01@users.noreply.github.com>
2021-11-10 23:20:27 +0700

    deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose
 
 const app = require('./app');
 
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 8080;
 app.listen(port, function (req, res) {
   console.log(`Listening on port ${port}....`);
 });
